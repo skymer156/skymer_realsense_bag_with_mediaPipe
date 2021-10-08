b80888e31bedd08d2d62093edf3cad14f056d0e2
skymer156 <46322826+skymer156@users.noreply.github.com>
2021-10-08 16:20:31 +0900

    Lider develop repository

---
+++ apps/config/config.py
@@ -0,0 +1,9 @@
+    """[summary]
+    project configulations difinition
+
+    keys = config_name
+    value = config_value
+    """
+dic_config = {
+    
+}
---
+++ apps/main.py
@@ -0,0 +1,8 @@
+import config.config
+
+def main():
+    print(config.dic_config)
+
+
+if __name__ == '__main__':
+    main()
